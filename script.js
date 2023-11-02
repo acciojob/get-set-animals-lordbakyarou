57ed77c32af7907f4347dc97beae5c635922d1bb
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-11-02 22:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
+class Animal {
+	
+	constructor(species){
+		this.species = species;
+	}
 
-class Dog extends Animal {}
+	get speciesName(){
+		return this.species;
+	}
+	
+	makeSound(){
+		
+	}
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof");
+	}
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
