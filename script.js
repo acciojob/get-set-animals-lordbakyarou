7faf0b252d2ad5dbf0c246b71110e4b257eb2bb9
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-11-02 23:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-//complete this code
 class Animal {
-	
-	constructor(species){
-		this.species = species;
-	}
+  constructor(species) {
+    this._species = species;
+  }
 
-	get speciesName(){
-		return this.species;
-	}
-	
-	makeSound(){
-		
-	}
-}
+  get species() {
+    return this._species;
+  }
 
-class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
+  makeSound(sound) {
+    console.log(`${this._species} makes a ${sound} sound.`);
+  }
 }
 
 class Cat extends Animal {
-	purr(){
-		console.log("purr");
-	}
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    this.makeSound("purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    this.makeSound("woof");
+  }
 }
 
 // Do not change the code below this line
